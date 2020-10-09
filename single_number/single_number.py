26442507e431d54ee1ed3ae841711a2c029deb52
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-10-08 22:16:30 -0400

    refactored single_number into while loop

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,15 +4,26 @@ Returns: an integer
 '''
 def single_number(arr):
     arr.sort()
-    for i in range(len(arr))[::2]:
-        if arr[i] == arr[-1]:
-            return arr[i]
-        if arr[i] == arr[i+1]:
-            continue
+    
+    if arr[-1] != arr[-2]:
+        return arr[-1]
+
+    i = 0
+    while i < len(arr):
+        num = arr[i]
+        next = arr[i+1]
+        if num == next:
+            i += 2
         else:
             return arr[i]
-
-    pass
+    
+    # for i in range(len(arr))[::2]:
+    #     if arr[i] == arr[-1]:
+    #         return arr[i]
+    #     if arr[i] == arr[i+1]:
+    #         continue
+    #     else:
+    #         return arr[i]
 
 
 if __name__ == '__main__':
